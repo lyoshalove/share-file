a061028f085711776a164f9fdf1f0b82814ec4e2
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-04-01 18:20:58 +0400

    refactor: remove cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,16 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173/',
-      'https://share-file-plum.vercel.app/',
-      'https://share-file-lyoshalove.vercel.app/',
-      'https://share-file-git-main-lyoshalove.vercel.app/',
-    ],
-    credentials: true,
-  });
-
   await app.listen(3000);
 }
 bootstrap();
