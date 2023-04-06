c396d3aa65d44fb139b0ca7318a3c199544b211d
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-04-06 19:33:28 +0400

    refactor: enable cors with origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://share-file-plum.vercel.app',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
